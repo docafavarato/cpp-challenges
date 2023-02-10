46d1cc22db0a444edbcd4a4f1cd3762d32c77dfc
terceirobapita <terceirobapitaa@gmail.com>
2023-02-10 00:22:53 -0300

    super mario

---
+++ Super Mario/main.cpp
@@ -0,0 +1,23 @@
+// Create a program that draws the super mario stairs based on a given amount of levels
+
+#include <iostream>
+using namespace std;
+
+string stairs(int levels) {
+    int count = 1;
+    for (int i = levels; i > 0; i--) {
+        if (i == levels) {
+            cout << string(i, ' ') << "?" << endl;
+            cout << string(i, ' ') << "|" << endl;
+        }
+        cout << string(i, ' ') << string(count, '#') << endl;
+        count++;
+    }
+
+    return "x";
+}
+
+int main() {
+    stairs(100);
+    return 0;
+}
